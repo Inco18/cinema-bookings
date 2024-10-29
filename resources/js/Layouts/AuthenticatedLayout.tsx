e1a393b2397f4621060f75291bf3c16d235635f1
Patryk Pacholczyk <patrykos190@gmail.com>
2024-10-29 14:44:04 +0100

    Add role based auth

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -27,12 +27,14 @@ export default function Authenticated({
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink
-                                    href={route("dashboard")}
-                                    active={route().current("dashboard")}
-                                >
-                                    Panel kontrolny
-                                </NavLink>
+                                {user.role == "admin" && (
+                                    <NavLink
+                                        href={route("dashboard")}
+                                        active={route().current("dashboard")}
+                                    >
+                                        Panel kontrolny
+                                    </NavLink>
+                                )}
                             </div>
                         </div>
 
